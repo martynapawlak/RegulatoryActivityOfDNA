2f2ee128c5f849971bfc38ab644e4a5f24a61ed6
martyna_pawlak <pawlakmartyna05@gmail.com>
2026-04-24 15:21:33 +0200

    changes in the training loop

---
+++ evaluation_script.py
@@ -0,0 +1,41 @@
+import torch
+import pandas as pd 
+import argparse
+import sys
+
+from models.model import DNAMultitaskModel
+from data.preprocess import one_hot_encode
+
+def evaluate(test_path, model_path):
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+	model = DNAMultitaskModel()
+	model.load_state_dict(torch.load(model_path, map_location=device))
+	model.to(device)
+	model.eval()
+	try:
+		df = pd.read_csv(test_path, sep='\t')
+	except Exception as e:
+		print(f"error while opening the file: {e}", file=sys.stderr)
+		return
+	print("id\tpredicted_is_active\tpredicted_rna_dna_ratio")
+	with torch.no_grad():
+		for index, row in df.iterrows():
+			seq_id = row['seq_id']
+			seq_str = row['sequence']
+			seq_tensor = one_hot_encode(seq_str).unsqueeze(0).to(device)
+			out_class, out_reg = model(seq_tensor)
+			class_prob = out_class.squeeze().item()
+			predicted_is_active = 1 if class_prob > 0.5 else 0
+			predicted_rna_dna_ratio = out_reg.squeeze().item()
+			print(f"{seq_id}\t{predicted_is_active}\t{predicted_rna_dna_ratio}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="evaluate cnn model")
+    parser.add_argument('test_path', type=str, help="path to test file")
+    parser.add_argument('model_path', type=str, help="path to model")
+    
+    args = parser.parse_args()
+    
+    evaluate(args.test_path, args.model_path)
+
--- training/train.py
+++ training/train.py
@@ -93,7 +93,8 @@ def training_loop_CNN(train_path, val_path, epochs, lr=0.0001, batch_size=64):
 
     if val_loss < best_val_loss:
       best_val_loss=val_loss
-      torch.save(model.state_dict(), 'dna_model.pt')
+      checkpoint={'model_state_dict': model.state_dict(),'train_mean': mean_reg,'train_std': std_reg}
+      torch.save(checkpoint.state_dict(), 'dna_model.pt')
       print(f"best new model saved !! :)")
 
     # computing the metrics
